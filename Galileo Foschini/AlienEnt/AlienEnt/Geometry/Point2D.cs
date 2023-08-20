f3a1310627e7c6ed70e30debc270838238a13472
Galileo Foschini <galileo.foschini@studio.unibo.it>
2023-08-20 21:53:22 +0200

    Fixed some errors in Point2D

--- Galileo Foschini/AlienEnt/AlienEnt/Geometry/Point2D.cs
+++ Galileo Foschini/AlienEnt/AlienEnt/Geometry/Point2D.cs
@@ -10,26 +10,25 @@ namespace AlienEnt.Geometry
             Y = y;
         }
 
-        public double X { get; private set; };
-        public double Y { get; private set;};
+        public double X { get; private set;}
+        public double Y { get; private set;}
 
-        public double distanceFrom(Point2D p) => Math.Sqrt(Math.Pow(X - p.X, 2) + Math.Pow(Y - p.Y, 2));
+        public double DistanceFrom(Point2D p) => Math.Sqrt(Math.Pow(X - p.X, 2) + Math.Pow(Y - p.Y, 2));
 
         public override string ToString() => $"Point2D [X = {X}, Y = {Y}]";
 
-        public override bool Equals(object obj) => obj is Point2D p &&
-                    X == p.X &&
-                    Y == p.Y;
+        public override bool Equals(object? obj)
+        {
+            return obj is Point2D d &&
+                   X == d.X &&
+                   Y == d.Y;
+        }
 
         public override int GetHashCode()
         {
-            //Codice generato tramite visual studio
-            var hashCode = -2050752806;
-            hashCode = hashCode * -1521134295 + X.GetHashCode();
-            hashCode = hashCode * -1521134295 + Y.GetHashCode();
-            return hashCode;
+            return HashCode.Combine(X, Y);
         }
 
-        public static readonly Point2D ORIGIN = new Point2D(0, 0);
+        public static readonly Point2D ORIGIN = new(0, 0);
     }
 }
