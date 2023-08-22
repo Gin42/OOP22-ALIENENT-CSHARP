9390ffae21035dacdfb6f5001c0193f010173052
Galileo Foschini <galileo.foschini@studio.unibo.it>
2023-08-22 12:12:53 +0200

    Added doc

--- Galileo Foschini/AlienEnt/AlienEnt/GameObject/Component/AbstractComponent.cs
+++ Galileo Foschini/AlienEnt/AlienEnt/GameObject/Component/AbstractComponent.cs
@@ -12,38 +12,45 @@ namespace AlienEnt.GameObject.Component
             _isEnabled = IsEnabled;
         }
 
+        /// <inheritdoc/>
         public void Disable()
         {
             _isEnabled = false;
         }
 
+        /// <inheritdoc/>
         public void Enable()
         {
             _isEnabled = true;
         }
 
+        /// <inheritdoc/>
         public bool IsEnabled()
         {
             return _isEnabled;
         }
 
+        /// <inheritdoc/>
         public IGameObject GetGameObject()
         {
             return _gameObject;
         }
 
+        /// <inheritdoc/>
         public virtual void Start()
         {
             // This method is empty on purpose,
             // so that not all Components must impement it
         }
 
+        /// <inheritdoc/>
         public virtual void Update(double deltaTime)
         {
             // This method is empty on purpose,
             // so that not all Components must impement it
         }
 
+        /// <inheritdoc/>
         public abstract IComponent? Duplicate(IGameObject obj);
     }
 }
--- Galileo Foschini/AlienEnt/AlienEnt/GameObject/GameObject.cs
+++ Galileo Foschini/AlienEnt/AlienEnt/GameObject/GameObject.cs
@@ -7,11 +7,18 @@ namespace AlienEnt.GameObject
     {
 
         private readonly Dictionary<Statistic, int> _stats;
-        private readonly List<IComponent> _components; 
+        private readonly List<IComponent> _components;
 
         private int _hp;
         private double _recoveryCooldown = 0;
 
+        /// <summary>
+        /// Contructor for all objects.
+        /// </summary>
+        /// <param name="position">Point2D of the position of the object.</param>
+        /// <param name="velocity">Vector2D of the velocity of the object</param>
+        /// <param name="stats">a dictionary with all the basic statistics of the object.</param>
+        /// <param name="id">the string id of the object.</param>
         public GameObject(Point2D position, Vector2D velocity, IDictionary<Statistic, int> stats, string id)
         {
             Position = position;
@@ -19,45 +26,55 @@ namespace AlienEnt.GameObject
             _stats = new Dictionary<Statistic, int>(stats);
             Id = id;
             _components = new List<IComponent>();
-            if(!_stats.TryGetValue(Statistic.HP, out _hp))
+            if (!_stats.TryGetValue(Statistic.HP, out _hp))
                 _hp = 0;
         }
 
+        /// <inheritdoc/>
         public Point2D Position { get; set; }
+        /// <inheritdoc/>
         public Vector2D Velocity { get; set; }
-        public string Id { get;}
+        /// <inheritdoc/>
+        public string Id { get; }
 
+        /// <inheritdoc/>
         public void AddAllComponents(ICollection<IComponent> components)
         {
             _components.AddRange(components);
         }
 
+        /// <inheritdoc/>
         public void AddComponent(IComponent component)
         {
             _components.Add(component);
         }
 
+        /// <inheritdoc/>
         public List<IComponent> GetAllComponents()
         {
             return new List<IComponent>(_components);
         }
 
+        /// <inheritdoc/>
         public Dictionary<Statistic, int> GetAllStats()
         {
             return new Dictionary<Statistic, int>(_stats);
         }
 
+        /// <inheritdoc/>
         public int GetHealth()
         {
             return _hp;
         }
 
+        /// <inheritdoc/>
         public int? GetStatValue(Statistic stat)
         {
             _stats.TryGetValue(stat, out int value);
             return value;
         }
 
+        /// <inheritdoc/>
         public void Heal(int heal)
         {
             if (heal < 0)
@@ -65,34 +82,40 @@ namespace AlienEnt.GameObject
             _hp += heal;
         }
 
+        /// <inheritdoc/>
         public void Hit(int damage)
         {
-            if(damage < 0)
+            if (damage < 0)
                 throw new ArgumentOutOfRangeException("the damage value must be positive");
             _hp -= damage;
         }
 
+        /// <inheritdoc/>
         public bool IsAlive()
         {
             return _hp > 0;
         }
 
+        /// <inheritdoc/>
         public void Recovery(double deltatime)
         {
-            if(_recoveryCooldown > 1)
+            if (_recoveryCooldown > 1)
             {
                 Heal(GetStatValue(Statistic.RECOVERY) ?? 0);
                 _recoveryCooldown = 0;
-            } else
+            }
+            else
                 _recoveryCooldown += deltatime;
         }
 
+        /// <inheritdoc/>
         public void SetStatValue(Statistic stat, int value)
         {
             if (!_stats.TryAdd(stat, value))
-                _stats[stat]=value;
+                _stats[stat] = value;
         }
-
+        
+        /// <inheritdoc/>
         public void Update(double deltaTime)
         {
             foreach (var component in _components)
