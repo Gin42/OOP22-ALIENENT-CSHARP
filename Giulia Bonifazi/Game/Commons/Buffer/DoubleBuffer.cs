5d2349d005f1206890b572e0c5581f7216934319
Giulia Bonifazi <giulia.bonifazi@studio.unibo.it>
2023-08-23 18:22:17 +0200

    Changes to respect conventions

--- Giulia Bonifazi/Game/Commons/Buffer/DoubleBuffer.cs
+++ Giulia Bonifazi/Game/Commons/Buffer/DoubleBuffer.cs
@@ -4,14 +4,13 @@ namespace Commons.Buffer {
     // Implementation of the DoubleBuffer interface.
     class DoubleBuffer<T> : IDoubleBuffer<T>
     {
-        public ISet<T> Buffer {private set; get;}
-
-        public ISet<T> Current {private set; get;}
-
-        public DoubleBuffer() {
+        public DoubleBuffer() 
+        {
             Current = new HashSet<T>();
             Buffer = new HashSet<T>();
         }
+        public ISet<T> Buffer {private set; get;}
+        public ISet<T> Current {private set; get;}
 
         public void ChangeBuffer()
         {
--- Giulia Bonifazi/Game/Commons/Point2D.cs
+++ Giulia Bonifazi/Game/Commons/Point2D.cs
@@ -1,16 +1,17 @@
 namespace Commons {
 
     public class Point2D {
+
+        public Point2D (double x, double y) {
+            X = x;
+            Y = y;
+        }
+        
         public double X { get; set; }
         public double Y { get; set; }
         public static Point2D Origin 
         {
             get => new(0.0, 0.0);
-        }
-
-        public Point2D (double x, double y) {
-            X = x;
-            Y = y;
         }
     }
 }
